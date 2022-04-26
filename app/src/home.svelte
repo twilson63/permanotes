63d167238349c18c9dbbcc69204dd587d4c0ef8e
Tom Wilson <tom@hyper.io>
2022-04-26 21:36:35 +0000

    updated learn more button on home screen

--- app/src/home.svelte
+++ app/src/home.svelte
@@ -9,7 +9,9 @@
       <p>Keep notes forever!</p>
       <div class="mt-16 space-x-8">
         <a href="/connect" class="btn btn-primary">Connect</a>
-        <a href="/learn" class="btn">Learn More</a>
+        <a href="/notes/Xx8lQw1q9xOUn1mB7CMagKHgv8XUy9NxsrQLtfqZItY" class="btn"
+          >Learn More</a
+        >
       </div>
     </div>
   </div>
