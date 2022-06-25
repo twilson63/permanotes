0adc96125daef7c2cb8da7c5e0a59a01dba3859d
Tom Wilson <tom@hyper.io>
2022-06-25 18:34:23 +0000

    adding profile wip

--- app/src/models/profiles.js
+++ app/src/models/profiles.js
@@ -8,19 +8,25 @@ import find from 'ramda/src/find'
 
 const schema = z.object({
   id: z.string().optional(),
-  type: z.literal('page').default('page'),
+  type: z.literal('profile').default('profile'),
   owner: z.string(),
-  contentType: z.literal('text/html').default('text/html'),
-  subdomain: z.string().max(20).optional(),
-  title: z.string().max(20),
-  description: z.string().max(50),
-  protocol: z.string().default('PermaPages-v0.3'),
-  content: z.string(),
-  html: z.string(),
-  profile: z.boolean().default(false),
-  weavemail: z.boolean().default(false),
-  ethwallet: z.string().default(''),
-  webpage: z.string().optional(),
+  avatar: z.string(),
+  name: z.string().max(20),
+  bio: z.string().max(100),
+  protocol: z.string().default('PermaProfile-v0.1'),
+  links: z.object({
+    etherium: z.string(),
+    solana: z.string(),
+    twitter: z.string(),
+    linkedIn: z.string(),
+    instagram: z.string(),
+    youtube: z.string(),
+    discord: z.string(),
+    twitch: z.string(),
+    facebook: z.string(),
+    background: z.string(),
+
+  }),
   timestamp: z.string(),
 })
 
@@ -29,7 +35,7 @@ const getTag = name => compose(
   find(propEq('name', name))
 )
 
-export const createPage = (data) => {
+export const createProfile = (data) => {
   data.timestamp = new Date().toISOString()
   const result = schema.safeParse(data)
   if (result.success) {
@@ -39,16 +45,17 @@ export const createPage = (data) => {
   }
 }
 
-export const txToPage = (tx) => {
+export const txToProfile = (tx) => {
   const tsValue = getTag('Timestamp')(tx.tags)
   const timestamp = tsValue ? tsValue : new Date().toISOString()
   const page = {
     id: tx.id,
     owner: tx.owner.address,
-    type: getTag('Type')(tx.tags) || 'page',
-    title: getTag('Page-Title')(tx.tags),
-    description: getTag('Description')(tx.tags),
-    webpage: getTag('Webpage')(tx.tags),
+    type: getTag('Type')(tx.tags) || 'profile',
+    name: getTag('Profile-Name')(tx.tags),
+    bio: getTag('Profile-Bio')(tx.tags),
+    webpage: getTag('Profile-Avatar')(tx.tags),
+
     timestamp
   }
   return page
